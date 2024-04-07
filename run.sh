1ac6d749134f66c9da896711c649858dea8b5d1b
Prabhvir Babra <theprabab@gmail.com>
2024-04-07 11:58:59 -0700

    Create a working urdf file for the arm

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker run -it -v "./ros2_ws":/ros2_ws --net=ros --device=/dev/ttyACM0 robot-arm bash
+sudo docker run -it -v "./catkin_ws":/catkin_ws --net=ros --device=/dev/ttyACM0 robot-arm bash
