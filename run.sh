84feeaed400075f7bbe29f90ec61cb9fc4401061
Prabhvir Babra <theprabab@gmail.com>
2024-06-03 23:17:21 -0700

    Convert this to a pure src repo

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker run -it -v "./catkin_ws":/catkin_ws --net=ros --device=/dev/ttyACM0 robot-arm bash
--- start_novnc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d --rm --net=ros \
-   --env="DISPLAY_WIDTH=2000" \
-   --env="DISPLAY_HEIGHT=1500" \
-   --env="RUN_XTERM=no" \
-   --name=novnc -p=8080:8080 \
-   theasp/novnc:latest
