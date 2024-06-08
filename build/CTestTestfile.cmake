3d2ffb3fefe30f881f0dc1ed0dcffa73b2dbb17c
Prabhvir <theprabab@gmail.com>
2024-06-08 23:11:23 +0000

    Checking

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /catkin_ws/src
-# Build directory: /catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("arm_keyboard_controller")
-subdirs("arm_sim")
--- build/arm_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /catkin_ws/src/arm_sim
-# Build directory: /catkin_ws/build/arm_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
